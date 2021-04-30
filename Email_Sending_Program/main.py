b55d0f28e54063196ed4ca72e307f31f0a1f782e
Thiago Sampaio <thiagosampaioparticular1@gmail.com>
2021-04-30 18:56:03 -0300

    V.3

--- Email_Sending_Program/main.py
+++ Email_Sending_Program/main.py
@@ -1,50 +1,69 @@
-import pandas as pd
+
 import smtplib
+from Data_Processing import Data_Processing
+from criar_lista import criar_lista
+
 from tkinter import *
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
+from email.mime.image import MIMEImage
 from email import encoders
+from tkinter import filedialog
+
 COR_BRANCA = "#ffffff"
 
-with open('CT AC.txt',"r") as file:
-    texto = file.readlines()
-    print(texto)
 #----------------------------- Preparação de dados ----------------------------#
 
-data = pd.read_csv("data1.csv", error_bad_lines=False, sep=";")
-lista_estados = list(data["estado"].unique())
-dict_data = {}
-for itens in lista_estados:
-    dict_data[itens] = list(data[data["estado"] == f"{itens}"]['email'])
+fp = open('imagem_pascoa.jpeg', 'rb')
 
-for (key,values) in dict_data.items():
-    print(key)
-print(dict_data)
-'''
-for (key,values) in dict_data.items():
-    if len(values) == 1:
-        # connection.sendmail(from_addr=my_email,to_addrs=values[0],msg=mensagem)
-    else:
-        for item in values:
-           #connectoin.sendmail(from_addr=my_mail,to_addrs=item,msg=mesagem)
-    print(len(values))
-'''
+msgImage = MIMEImage(fp.read())
+
+fp.close()
+
+def UploadAction_File():
+    file = filedialog.askopenfilename()
+    global lista_estados, dict_data
+    lista_estados, dict_data = Data_Processing(file)
+    criar_lista(lista_estados)
+
+
+def Upload_Imagens():
+    numero_anexos = int(N_anexos_entry.get())
+    dicionario_imagens = {}
+    for i in range(1, numero_anexos + 1):
+        file = filedialog.askopenfilename()
+        fp = open(file,"rb")
+        dicionario_imagens[f"image{i}"] = MIMEImage(fp.read())
+        fp.close()
+    return dicionario_imagens
+
+
+
+
+
 def mandar_email():
+    dicionario_imagens = Upload_Imagens()
 
     fromaddr = fromaddr_entry.get()
     Subject_message = Subject_entry.get()
-    corpo_message = corpo_entry.get()
+    senha = corpo_entry.get()
+
 
     for (key,values) in dict_data.items():
         if len(values) == 1:
+            with open (f"Cartas_txt/CT {key}.txt",encoding="utf8") as file:
+                corpo_message = file.readlines()
+                corpo_message_txt = "".join(corpo_message)
             toaddr = values[0]
             msg = MIMEMultipart()
             msg['From'] = fromaddr
             msg['To'] = values[0]
             msg['Subject'] = Subject_message
-            body = corpo_message
+            body = corpo_message_txt
             msg.attach(MIMEText(body, 'plain'))
+            msg.attach(msgImage)
+            #msg.attach(part)
             estado = str(key).upper()
             filename = f"CT {estado}.doc"
             attachment = open(f"Cartas/CT {estado}.doc", "rb")
@@ -52,10 +71,12 @@ def mandar_email():
             p.set_payload((attachment).read())
             encoders.encode_base64(p)
             p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
-            msg.attach(p)
+            #msg.attach(p)
+            for item in dicionario_imagens.values():
+                msg.attach(item)
             s = smtplib.SMTP('smtp.gmail.com', 587)
             s.starttls()
-            s.login(fromaddr, "312907Thiago")
+            s.login(fromaddr, senha)
             text = msg.as_string()
             s.sendmail(fromaddr, toaddr, text)
             s.quit()
@@ -66,7 +87,10 @@ def mandar_email():
                 msg['From'] = fromaddr
                 msg['To'] = item
                 msg['Subject'] = Subject_message
-                body = corpo_message
+                with open(f"Cartas_txt/CT {key}.txt", encoding="utf8") as file:
+                    corpo_message = file.readlines()
+                    corpo_message_txt = "".join(corpo_message)
+                body = corpo_message_txt
                 msg.attach(MIMEText(body, 'plain'))
                 estado = str(key).upper()
                 filename = f"CT {estado}.doc"
@@ -75,10 +99,14 @@ def mandar_email():
                 p.set_payload((attachment).read())
                 encoders.encode_base64(p)
                 p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
-                msg.attach(p)
+                #msg.attach(p)
+                #msg.attach(part)
+                for item in dicionario_imagens.values():
+                    msg.attach(item)
+                #msg.attach(msgImage)
                 s = smtplib.SMTP('smtp.gmail.com', 587)
                 s.starttls()
-                s.login(fromaddr, "312907Thiago")
+                s.login(fromaddr, senha)
                 text = msg.as_string()
                 s.sendmail(fromaddr, toaddr, text)
                 s.quit()
@@ -92,9 +120,9 @@ window.config(padx=20, pady=20, bg=COR_BRANCA)
 
 # # -- CANVAS SETUP -- #
 
-canvas = Canvas(width=400, height=400, highlightthickness=0)
-logo_img=PhotoImage(file='logo_empresa.png')
-canvas.create_image(200, 200, image=logo_img)
+canvas = Canvas(width=1195, height=500, highlightthickness=0)
+#logo_img=PhotoImage(file='logo/logo.jpg')
+#canvas.create_image(597, 250, image=logo_img)
 canvas.config(bg=COR_BRANCA, highlightthickness=0)
 canvas.grid(row=1, column=1)
 
@@ -103,11 +131,13 @@ fromaddr_label = Label(text="EMAIL REMETENTE(gmail):", font=("Arial", 10, "bold"
 fromaddr_label.grid(column=0, row=2)
 
 Subject_label = Label(text="ASSUNTO DO EMAIL:", font=("Arial", 10, "bold"),bg=COR_BRANCA)
-Subject_label.grid(column=0, row=3)
+Subject_label.grid(column=0, row=4)
 
-corpo_label = Label(text="CORPO DO EMAIL:", font=("Arial", 10, "bold"),bg=COR_BRANCA)
-corpo_label.grid(column=0, row=4)
+corpo_label = Label(text="SENHA DO EMAIL:", font=("Arial", 10, "bold"),bg=COR_BRANCA)
+corpo_label.grid(column=0, row=3)
 
+N_anexos= Label(text="Numero de anexos", font=("Arial", 10, "bold"),bg=COR_BRANCA)
+N_anexos.grid(column=0, row=5)
 
 # -- ENTRY -- #
 fromaddr_entry = Entry(width=35, bg='#cccccc')
@@ -115,18 +145,25 @@ fromaddr_entry.grid(column=0, row=2, columnspan=2)
 fromaddr_entry.focus()
 
 Subject_entry = Entry(width=35, bg='#cccccc')
-Subject_entry.grid(column=0, row=3, columnspan=2)
+Subject_entry.grid(column=0, row=4, columnspan=2)
 
 corpo_entry = Entry(width=35, bg='#cccccc')
-corpo_entry.grid(column=0, row=4, columnspan=2)
+corpo_entry.grid(column=0, row=3, columnspan=2)
+
+N_anexos_entry = Entry(width=35, bg='#cccccc')
+N_anexos_entry .grid(column=0, row=5, columnspan=2)
 
 
 # -- BUTTON SETUP -- #
 
 # # -------------------- BUTTON 1 SETUP ------------------------- # #
+
+button = Button(text='Abrir arquivo ".CSV"', command=UploadAction_File)
+button.grid(column=0, row=1)
+
+# # -------------------- BUTTON 2 SETUP ------------------------- # #
 generate_password_button = Button(text="Mandar email", highlightthickness=0, command=mandar_email)
 generate_password_button.grid(column=2, row=2)
-
 
 
 window.mainloop()
